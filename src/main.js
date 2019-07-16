75f4fa4096b2b5ce4a6b055a2248ebc958db4955
C-D-Lewis <Bonsitm@gmail.com>
2019-07-16 10:38:58 +0100

    Better handle different types of errors

--- src/main.js
+++ src/main.js
@@ -33,14 +33,28 @@ const main = async () => {
     await command.execute(args.slice(1));
   } catch (e) {
     const { errorDetail } = config.get('options');
+
+    // Transform fetch error response
+    if (typeof e.ok !== 'undefined' && !e.ok) {
+      e = await e.json().catch(() => e.text());
+    }
+
+    // API error response
     if (e.errors) {
       const errStr = errorDetail ? JSON.stringify(e, null, 2) : e.errors[0];
       logger.error(`\nEVRYTHNG Error (${e.status}): ${errStr}`);
       return;
     }
 
-    const errStr = errorDetail ? e.stack : e.message;
-    logger.error(`\n${errStr}`);
+    // Native error
+    if (e.message) {
+      const errStr = errorDetail ? e.stack : e.message;
+      logger.error(`\n${errStr}`);
+      return;
+    }
+
+    // Some other unknown type
+    console.log(e);
   }
 };
 
