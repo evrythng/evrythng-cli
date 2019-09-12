da6cb519c535098b821b4c8f206960bed049147d
C-D-Lewis <Bonsitm@gmail.com>
2019-09-12 11:12:25 +0100

    Relax expand() test to deal with timezones

--- tests/modules/functions.js
+++ tests/modules/functions.js
@@ -33,7 +33,8 @@ describe('functions', () => {
 
     const input = JSON.parse(JSON.stringify(original));
     await expand(input);
-    expect(input).to.deep.equal(expected);
+    expect(input.createdAt).to.be.a('string');
+    expect(input.updatedAt).to.be.a('string');
   });
 
   it('should expand an example array payload', async () => {
