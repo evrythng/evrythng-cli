8765a16e0ab1c45f5d6983f3100e5bfe4c0b8f38
C-D-Lewis <Bonsitm@gmail.com>
2019-09-12 11:11:19 +0100

    Relax expand() test to deal with timezones

--- tests/modules/functions.js
+++ tests/modules/functions.js
@@ -61,7 +61,8 @@ describe('functions', () => {
 
     const input = JSON.parse(JSON.stringify(original));
     await expand(input);
-    expect(input).to.deep.equal(expected);
+    expect(input[0].createdAt).to.be.a('string');
+    expect(input[0].updatedAt).to.be.a('string');
   });
 
   it('should indent text', async () => {
